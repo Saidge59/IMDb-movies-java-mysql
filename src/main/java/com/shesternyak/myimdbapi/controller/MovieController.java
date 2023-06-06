cc6eb2fa772f3f0bbdd4077ffcd82ff6b4614476
Denis Shesternyak <shesternyakdenis@gmail.com>
2023-06-06 21:30:13 +0300

    Adding functionl to 'MovieController' and most-popular-movies.html

--- src/main/java/com/shesternyak/myimdbapi/controller/MovieController.java
+++ src/main/java/com/shesternyak/myimdbapi/controller/MovieController.java
@@ -1,13 +1,30 @@
 package com.shesternyak.myimdbapi.controller;
 
+import com.shesternyak.myimdbapi.dto.MovieDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class MovieController {
 
     @RequestMapping("/most-popular-movies")
-    public String getMostPopularMovies(){
+    public String getMostPopularMovies(Model model) {
+        MovieDTO movie1 = new MovieDTO("tt5971474", "The Little Mermaid", 2023, "https://m.media-amazon.com/images/M/MV5BYTUxYjczMWUtYzlkZC00NTcwLWE3ODQtN2I2YTIxOTU0ZTljXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_Ratio0.6716_AL_.jpg", "Rob Marshall (dir.), Halle Bailey, Jonah Hauer-King", 7.2f);
+        MovieDTO movie2 = new MovieDTO("tt9362722", "Spider-Man: Across the Spider-Verse", 2023, "https://m.media-amazon.com/images/M/MV5BNzQ1ODUzYjktMzRiMS00ODNiLWI4NzQtOTRiN2VlNTNmODFjXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_Ratio0.6716_AL_.jpg", "Joaquim Dos Santos (dir.), Shameik Moore, Hailee Steinfeld", 9.1f);
+        MovieDTO movie3 = new MovieDTO("tt10366206", "John Wick: Chapter 4", 2023, "https://m.media-amazon.com/images/M/MV5BMDExZGMyOTMtMDgyYi00NGIwLWJhMTEtOTdkZGFjNmZiMTEwXkEyXkFqcGdeQXVyMjM4NTM5NDY@._V1_Ratio0.6716_AL_.jpg", "Chad Stahelski (dir.), Keanu Reeves, Laurence Fishburne", 8.0f);
+        MovieDTO movie4 = new MovieDTO("tt1517268", "Barbie", 2023, "https://m.media-amazon.com/images/M/MV5BOWIwZGY0OTYtZjUzYy00NzRmLTg5YzgtYWMzNWQ0MmZiY2MwXkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_Ratio0.6716_AL_.jpg", "Greta Gerwig (dir.), Margot Robbie, Ryan Gosling", 0.0f);
+
+        List<MovieDTO> movies = new ArrayList<>();
+        movies.add(movie1);
+        movies.add(movie2);
+        movies.add(movie3);
+        movies.add(movie4);
+
+        model.addAttribute("movies", movies);
         return "most-popular-movies";
     }
 }
--- src/main/java/com/shesternyak/myimdbapi/dto/MovieDTO.java
+++ src/main/java/com/shesternyak/myimdbapi/dto/MovieDTO.java
@@ -1,5 +1,6 @@
 package com.shesternyak.myimdbapi.dto;
 
+import com.shesternyak.myimdbapi.domain.Movie;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,4 +15,13 @@ public class MovieDTO {
     private String image;
     private String crew;
     private float imDbRating;
+
+    public MovieDTO(Movie movie) {
+        this.id = movie.getId();
+        this.title = movie.getTitle();
+        this.year = movie.getYear();
+        this.image = movie.getImage();
+        this.crew = movie.getCrew();
+        this.imDbRating = movie.getImDbRating();
+    }
 }
