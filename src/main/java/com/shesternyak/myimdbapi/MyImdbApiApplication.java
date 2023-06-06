51731796245c6bbb8c2e5b06acdcd101659f5d78
Denis Shesternyak <shesternyakdenis@gmail.com>
2023-06-06 14:36:19 +0300

    Init

---
+++ src/main/java/com/shesternyak/myimdbapi/MyImdbApiApplication.java
@@ -0,0 +1,13 @@
+package com.shesternyak.myimdbapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyImdbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyImdbApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shesternyak/myimdbapi/MyImdbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shesternyak.myimdbapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyImdbApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
