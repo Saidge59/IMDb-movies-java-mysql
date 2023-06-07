e1502755f542f163ad73a6aadfe965136f3f14f5
Denis Shesternyak <shesternyakdenis@gmail.com>
2023-06-07 14:37:42 +0300

    The MovieController.java has been updated

--- src/main/java/com/shesternyak/myimdbapi/controller/MovieController.java
+++ src/main/java/com/shesternyak/myimdbapi/controller/MovieController.java
@@ -1,6 +1,8 @@
 package com.shesternyak.myimdbapi.controller;
 
+import com.shesternyak.myimdbapi.domain.Movie;
 import com.shesternyak.myimdbapi.dto.MovieDTO;
+import com.shesternyak.myimdbapi.service.MoviesService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +13,14 @@ import java.util.List;
 @Controller
 public class MovieController {
 
-    @RequestMapping("/most-popular-movies")
-    public String getMostPopularMovies(Model model) {
+    MoviesService moviesService;
+
+    public MovieController(MoviesService moviesService) {
+        this.moviesService = moviesService;
+    }
+
+    @RequestMapping("/test-movie")
+    public String testView(Model model) {
         MovieDTO movie1 = new MovieDTO("tt5971474", "The Little Mermaid", 2023, "https://m.media-amazon.com/images/M/MV5BYTUxYjczMWUtYzlkZC00NTcwLWE3ODQtN2I2YTIxOTU0ZTljXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_Ratio0.6716_AL_.jpg", "Rob Marshall (dir.), Halle Bailey, Jonah Hauer-King", 7.2f);
         MovieDTO movie2 = new MovieDTO("tt9362722", "Spider-Man: Across the Spider-Verse", 2023, "https://m.media-amazon.com/images/M/MV5BNzQ1ODUzYjktMzRiMS00ODNiLWI4NzQtOTRiN2VlNTNmODFjXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_Ratio0.6716_AL_.jpg", "Joaquim Dos Santos (dir.), Shameik Moore, Hailee Steinfeld", 9.1f);
         MovieDTO movie3 = new MovieDTO("tt10366206", "John Wick: Chapter 4", 2023, "https://m.media-amazon.com/images/M/MV5BMDExZGMyOTMtMDgyYi00NGIwLWJhMTEtOTdkZGFjNmZiMTEwXkEyXkFqcGdeQXVyMjM4NTM5NDY@._V1_Ratio0.6716_AL_.jpg", "Chad Stahelski (dir.), Keanu Reeves, Laurence Fishburne", 8.0f);
@@ -25,6 +33,15 @@ public class MovieController {
         movies.add(movie4);
 
         model.addAttribute("movies", movies);
+        return "most-popular-movies";
+    }
+
+    @RequestMapping("/most-popular-movies")
+    public String getMostPopularMovies(Model model) {
+        List<Movie> mostPopularMovies = moviesService.getMostPopularMovies();
+        List<MovieDTO> mpmDTO = moviesService.convertMovieToMovieDTO(mostPopularMovies);
+
+        model.addAttribute("movies", mpmDTO);
         return "most-popular-movies";
     }
 }
