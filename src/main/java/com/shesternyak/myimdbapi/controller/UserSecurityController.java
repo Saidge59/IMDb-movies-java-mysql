4605aeb2a947f00140c6f5070a5410ba9791570e
Denis Shesternyak <shesternyakdenis@gmail.com>
2023-06-08 10:19:53 +0300

    Adding methods get and post for '/registration'

--- src/main/java/com/shesternyak/myimdbapi/controller/UserSecurityController.java
+++ src/main/java/com/shesternyak/myimdbapi/controller/UserSecurityController.java
@@ -1,12 +1,59 @@
 package com.shesternyak.myimdbapi.controller;
 
+import com.shesternyak.myimdbapi.dto.UserRegistrationDTO;
+import com.shesternyak.myimdbapi.service.registration.UserService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class UserSecurityController {
+    private UserService userService;
+
+    public UserSecurityController(UserService userService) {
+        this.userService = userService;
+    }
+
     @RequestMapping("/login")
     public String login(){
         return "login";
+    }
+
+    @ModelAttribute("user")
+    public UserRegistrationDTO userRegistrationDTO() {
+        return new UserRegistrationDTO();
+    }
+
+    @GetMapping("/registration")
+    public String showRegistrationMapping() {
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String registerUserAccount(@Valid @ModelAttribute("user") UserRegistrationDTO registrationDTO,
+                                      BindingResult result,
+                                      RedirectAttributes redirectAttributes) {
+
+        if (result.hasErrors()) {
+            return "registration";
+        }
+
+        if (userService.findUserByName(registrationDTO.getName()) != null) {
+            redirectAttributes.addFlashAttribute("errorUsername", registrationDTO.getName());
+            return "redirect:/registration";
+        } else if (userService.findByEmail(registrationDTO.getEmail()) != null) {
+            redirectAttributes.addFlashAttribute("errorEmail", registrationDTO.getEmail());
+            return "redirect:/registration";
+        }
+
+        userService.save(registrationDTO);
+
+        redirectAttributes.addFlashAttribute("success", "");
+        return "redirect:/registration";
     }
 }
