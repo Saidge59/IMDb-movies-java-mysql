464d2315305050d20787a2e6173415cc96a26b49
Denis Shesternyak <shesternyakdenis@gmail.com>
2023-06-08 09:57:16 +0300

    Creating 'Role'

---
+++ src/main/java/com/shesternyak/myimdbapi/domain/registration/Role.java
@@ -0,0 +1,27 @@
+package com.shesternyak.myimdbapi.domain.registration;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+}
