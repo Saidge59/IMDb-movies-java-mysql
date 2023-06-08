5e16cb7ab001a05e5ac569f0d8071fad5a803b20
Denis Shesternyak <shesternyakdenis@gmail.com>
2023-06-08 09:56:46 +0300

    Adding dependensy data-jpa to pom.xml

--- src/main/java/com/shesternyak/myimdbapi/domain/registration/User.java
+++ src/main/java/com/shesternyak/myimdbapi/domain/registration/User.java
@@ -1,2 +1,47 @@
-package com.shesternyak.myimdbapi.domain.registration;public class User {
+package com.shesternyak.myimdbapi.domain.registration;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @ManyToMany(cascade = {CascadeType.MERGE, CascadeType.DETACH, CascadeType.PERSIST, CascadeType.REFRESH}, fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
+    private List<Role> roles;
+
+    public User(String name, String email, String password, List<Role> roles) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.roles = roles;
+    }
+
 }
