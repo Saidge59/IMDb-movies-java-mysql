0627f3e792cb2a34ca1c08f3032656ba020a1ed4
Denis Shesternyak <shesternyakdenis@gmail.com>
2023-06-08 19:31:18 +0300

    Updated 'Convertor'

--- src/main/java/com/shesternyak/myimdbapi/system/Convertor.java
+++ src/main/java/com/shesternyak/myimdbapi/system/Convertor.java
@@ -9,14 +9,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Convertor {
-//    public static <T extends Movie> List<MovieDTO> convertMovieTToMovieDTO(List<T> movies) {
-//        List<MovieDTO> movieDTOs = new ArrayList<>();
-//        for (T m : movies) {
-//            MovieDTO movieDTO = new MovieDTO(m.getId(), m.getTitle(), m.getYear(), m.getImage(), m.getCrew(), m.getImDbRating());
-//            movieDTOs.add(movieDTO);
-//        }
-//        return movieDTOs;
-//    }
 
     public static List<MovieDTO> convertMovieToMovieDTO(List<Movie> movies) {
         List<MovieDTO> movieDTOs = new ArrayList<>();
@@ -43,5 +35,13 @@ public class Convertor {
             movieDTOs.add(movieDTO);
         }
         return movieDTOs;
+    }
+
+    public static MovieSaved convertMovieDTOToMovieSaved(MovieDTO movie) {
+        return new MovieSaved(movie.getId(), movie.getTitle(), movie.getYear(), movie.getImage(), movie.getCrew(), movie.getImDbRating());
+    }
+
+    public static MovieFavorites convertMovieDTOToMovieFavorites(MovieDTO movie) {
+        return new MovieFavorites(movie.getId(), movie.getTitle(), movie.getYear(), movie.getImage(), movie.getCrew(), movie.getImDbRating());
     }
 }
