9c0c94dfcfe718d2fea4f3e9013f44d5d987af03
Denis Shesternyak <shesternyakdenis@gmail.com>
2023-06-08 10:13:58 +0300

    Fixed imports in RoleRepository

--- src/main/java/com/shesternyak/myimdbapi/repo/RoleRepository.java
+++ src/main/java/com/shesternyak/myimdbapi/repo/RoleRepository.java
@@ -1,7 +1,7 @@
 package com.shesternyak.myimdbapi.repo;
 
-import com.example.films_ratings.entity.registration.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.shesternyak.myimdbapi.domain.registration.Role;
 
 public interface RoleRepository extends JpaRepository<Role, Integer> {
     public Role findByName(String name);
