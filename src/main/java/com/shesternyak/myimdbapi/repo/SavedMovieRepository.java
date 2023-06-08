f588665d00f5b63a8ad98fa105e95d7cae626b63
Denis Shesternyak <shesternyakdenis@gmail.com>
2023-06-08 17:52:44 +0300

    Creating the interface 'SavedMoviesRepository'

---
+++ src/main/java/com/shesternyak/myimdbapi/repo/SavedMovieRepository.java
@@ -0,0 +1,7 @@
+package com.shesternyak.myimdbapi.repo;
+
+import com.shesternyak.myimdbapi.domain.MovieSaved;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SavedMovieRepository extends JpaRepository<MovieSaved, Integer> {
+}
