21bc449681cd9ee0599cb3507c1f8b14d8ab2717
Denis Shesternyak <shesternyakdenis@gmail.com>
2023-06-08 10:11:13 +0300

    Creating interface UserRepository

---
+++ src/main/java/com/shesternyak/myimdbapi/repo/UserRepository.java
@@ -0,0 +1,10 @@
+package com.shesternyak.myimdbapi.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.shesternyak.myimdbapi.domain.registration.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByName(String name);
+
+    User findByEmail(String name);
+}
