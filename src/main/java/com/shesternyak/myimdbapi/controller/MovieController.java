d7c67f5465ee92d18ed167aa599d1560ea762362
Denis Shesternyak <shesternyakdenis@gmail.com>
2023-06-09 14:49:14 +0300

    Changed class 'MovieController' some methods

--- src/main/java/com/shesternyak/myimdbapi/controller/MovieController.java
+++ src/main/java/com/shesternyak/myimdbapi/controller/MovieController.java
@@ -1,80 +1,94 @@
 package com.shesternyak.myimdbapi.controller;
 
 import com.shesternyak.myimdbapi.domain.Movie;
+import com.shesternyak.myimdbapi.domain.MovieDB;
 import com.shesternyak.myimdbapi.dto.MovieDTO;
+import com.shesternyak.myimdbapi.service.MovieService;
 import com.shesternyak.myimdbapi.service.MoviesFromIMDdService;
 import com.shesternyak.myimdbapi.system.Convertor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class MovieController {
 
     MoviesFromIMDdService moviesService;
+    MovieService movieService;
 
-    public MovieController(MoviesFromIMDdService moviesService) {
+    public MovieController(MoviesFromIMDdService moviesService, MovieService movieService) {
         this.moviesService = moviesService;
+        this.movieService = movieService;
     }
 
-    @RequestMapping("/test-movie")
-    public String testView(Model model) {
-        MovieDTO movie1 = new MovieDTO("tt5971474", "The Little Mermaid", 2023, "https://m.media-amazon.com/images/M/MV5BYTUxYjczMWUtYzlkZC00NTcwLWE3ODQtN2I2YTIxOTU0ZTljXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_UX300_CR0,11,300,400_AL_.jpg", "Rob Marshall (dir.), Halle Bailey, Jonah Hauer-King", 7.2f);
-        MovieDTO movie2 = new MovieDTO("tt9362722", "Spider-Man: Across the Spider-Verse", 2023, "https://m.media-amazon.com/images/M/MV5BNzQ1ODUzYjktMzRiMS00ODNiLWI4NzQtOTRiN2VlNTNmODFjXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_UX300_CR0,11,300,400_AL_.jpg", "Joaquim Dos Santos (dir.), Shameik Moore, Hailee Steinfeld", 9.1f);
-        MovieDTO movie3 = new MovieDTO("tt10366206", "John Wick: Chapter 4", 2023, "https://m.media-amazon.com/images/M/MV5BMDExZGMyOTMtMDgyYi00NGIwLWJhMTEtOTdkZGFjNmZiMTEwXkEyXkFqcGdeQXVyMjM4NTM5NDY@._V1_UX300_CR0,11,300,400_AL_.jpg", "Chad Stahelski (dir.), Keanu Reeves, Laurence Fishburne", 8.0f);
-        MovieDTO movie4 = new MovieDTO("tt1517268", "Barbie", 2023, "https://m.media-amazon.com/images/M/MV5BOWIwZGY0OTYtZjUzYy00NzRmLTg5YzgtYWMzNWQ0MmZiY2MwXkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_UX300_CR0,11,300,400_AL_.jpg", "Greta Gerwig (dir.), Margot Robbie, Ryan Gosling", 0.0f);
-
-        List<MovieDTO> movies = new ArrayList<>();
-        movies.add(movie1);
-        movies.add(movie2);
-        movies.add(movie3);
-        movies.add(movie4);
-
-        model.addAttribute("movies", movies);
-
-        return "list-movies";
-    }
-
-    @RequestMapping("/most-popular-movies")
+    @GetMapping("/most-popular-movies")
     public String getMostPopularMovies(Model model) {
         List<Movie> mostPopularMovies = moviesService.getMostPopularMovies();
-        List<MovieDTO> mpmDTO = Convertor.convertMovieToMovieDTO(mostPopularMovies);
+        List<MovieDTO> tempList = Convertor.convertMovieToMovieDTO(mostPopularMovies);
 
-        model.addAttribute("movies", mpmDTO);
+        List<MovieDB> movieDBList = movieService.getAllMovies();
+        List<MovieDTO> repoList = Convertor.convertMovieDBToMovieDTO(movieDBList);
 
+        checkSavedAndFavorites(tempList,  repoList);
+
+        model.addAttribute("movies", tempList);
         return "most-popular-movies";
     }
 
-    @RequestMapping("/most-popular-series")
+    @GetMapping("/most-popular-series")
     public String getMostPopularSeries(Model model) {
         List<Movie> mostPopularSeries = moviesService.getMostPopularSeries();
-        List<MovieDTO> mpsDTO = Convertor.convertMovieToMovieDTO(mostPopularSeries);
+        List<MovieDTO> tempList = Convertor.convertMovieToMovieDTO(mostPopularSeries);
 
-        model.addAttribute("movies", mpsDTO);
+        List<MovieDB> movieDBList = movieService.getAllMovies();
+        List<MovieDTO> repoList = Convertor.convertMovieDBToMovieDTO(movieDBList);
 
+        checkSavedAndFavorites(tempList,  repoList);
+
+        model.addAttribute("movies", tempList);
         return "most-popular-series";
     }
 
-    @RequestMapping("/top-250-movies")
+    @GetMapping("/top-250-movies")
     public String getTop250Movies(Model model) {
         List<Movie> top250Movies = moviesService.getTop250Movies();
-        List<MovieDTO> top250DTO = Convertor.convertMovieToMovieDTO(top250Movies);
+        List<MovieDTO> tempList = Convertor.convertMovieToMovieDTO(top250Movies);
 
-        model.addAttribute("movies", top250DTO);
+        List<MovieDB> movieDBList = movieService.getAllMovies();
+        List<MovieDTO> repoList = Convertor.convertMovieDBToMovieDTO(movieDBList);
 
+        checkSavedAndFavorites(tempList,  repoList);
+
+        model.addAttribute("movies", tempList);
         return "top-250-movies";
     }
 
-    @RequestMapping("/top-250-series")
+    @GetMapping("/top-250-series")
     public String getTop250Series(Model model) {
         List<Movie> top250Series = moviesService.getTop250Series();
-        List<MovieDTO> top250DTO = Convertor.convertMovieToMovieDTO(top250Series);
+        List<MovieDTO> tempList = Convertor.convertMovieToMovieDTO(top250Series);
 
-        model.addAttribute("movies", top250DTO);
+        List<MovieDB> movieDBList = movieService.getAllMovies();
+        List<MovieDTO> repoList = Convertor.convertMovieDBToMovieDTO(movieDBList);
 
+        checkSavedAndFavorites(tempList,  repoList);
+
+        model.addAttribute("movies", tempList);
         return "top-250-series";
+    }
+
+    private void checkSavedAndFavorites(List<MovieDTO> tempList, List<MovieDTO> repoList) {
+        if (!repoList.isEmpty()) {
+            for (MovieDTO t : tempList) {
+                for (MovieDTO r : repoList) {
+                    if(r.getId().equals(t.getId())) {
+                        t.setSaved(r.isSaved());
+                        t.setFavorites(r.isFavorites());
+                    }
+                }
+            }
+        }
     }
 }
