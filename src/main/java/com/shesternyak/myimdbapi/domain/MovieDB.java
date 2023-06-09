981ba0701e32c29b847ad8f4624a0ab6f3b87c06
Denis Shesternyak <shesternyakdenis@gmail.com>
2023-06-09 12:36:01 +0300

    Modified the 'MovieDB'

--- src/main/java/com/shesternyak/myimdbapi/domain/MovieDB.java
+++ src/main/java/com/shesternyak/myimdbapi/domain/MovieDB.java
@@ -12,11 +12,25 @@ import lombok.ToString;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-public class Movie {
+@Entity
+@Table(name = "movies_db")
+public class MovieDB {
+    @Id
     private String id;
     private String title;
     private int year;
     private String image;
     private String crew;
     private float imDbRating;
+    private boolean isSaved;
+    private boolean isFavorites;
+
+    public MovieDB(String id, String title, int year, String image, String crew, float imDbRating) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.image = image;
+        this.crew = crew;
+        this.imDbRating = imDbRating;
+    }
 }
