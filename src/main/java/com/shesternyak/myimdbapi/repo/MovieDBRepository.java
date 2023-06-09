6add1e0e3fb3607bc1219c061438ba05ac3730e4
Denis Shesternyak <shesternyakdenis@gmail.com>
2023-06-09 12:36:28 +0300

    Modified the 'MovieDBRepository'

--- src/main/java/com/shesternyak/myimdbapi/repo/MovieDBRepository.java
+++ src/main/java/com/shesternyak/myimdbapi/repo/MovieDBRepository.java
@@ -1,8 +1,9 @@
 package com.shesternyak.myimdbapi.repo;
 
-import com.shesternyak.myimdbapi.domain.MovieFavorites;
+import com.shesternyak.myimdbapi.domain.MovieDB;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FavoritesMovieRepository extends JpaRepository<MovieFavorites, Integer> {
+public interface MovieDBRepository extends JpaRepository<MovieDB, String> {
+    MovieDB getById(String id);
     void deleteById(String id);
 }
