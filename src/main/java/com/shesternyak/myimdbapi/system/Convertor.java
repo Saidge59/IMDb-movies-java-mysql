0a4648fc8ded2f0ffd9a436a0f8018d4ae44992b
Denis Shesternyak <shesternyakdenis@gmail.com>
2023-06-09 14:51:42 +0300

    Changed class 'Convertor' some methods

--- src/main/java/com/shesternyak/myimdbapi/system/Convertor.java
+++ src/main/java/com/shesternyak/myimdbapi/system/Convertor.java
@@ -2,8 +2,6 @@ package com.shesternyak.myimdbapi.system;
 
 import com.shesternyak.myimdbapi.domain.Movie;
 import com.shesternyak.myimdbapi.domain.MovieDB;
-import com.shesternyak.myimdbapi.domain.MovieFavorites;
-import com.shesternyak.myimdbapi.domain.MovieSaved;
 import com.shesternyak.myimdbapi.dto.MovieDTO;
 
 import java.util.ArrayList;
@@ -18,32 +16,6 @@ public class Convertor {
             movieDTOs.add(movieDTO);
         }
         return movieDTOs;
-    }
-
-    public static List<MovieDTO> convertMovieSavedToMovieDTO(List<MovieSaved> movies) {
-        List<MovieDTO> movieDTOs = new ArrayList<>();
-        for (MovieSaved m : movies) {
-            MovieDTO movieDTO = new MovieDTO(m.getId(), m.getTitle(), m.getYear(), m.getImage(), m.getCrew(), m.getImDbRating());
-            movieDTOs.add(movieDTO);
-        }
-        return movieDTOs;
-    }
-
-    public static List<MovieDTO> convertMovieFavoritesToMovieDTO(List<MovieFavorites> movies) {
-        List<MovieDTO> movieDTOs = new ArrayList<>();
-        for (MovieFavorites m : movies) {
-            MovieDTO movieDTO = new MovieDTO(m.getId(), m.getTitle(), m.getYear(), m.getImage(), m.getCrew(), m.getImDbRating());
-            movieDTOs.add(movieDTO);
-        }
-        return movieDTOs;
-    }
-
-    public static MovieSaved convertMovieDTOToMovieSaved(MovieDTO movie) {
-        return new MovieSaved(movie.getId(), movie.getTitle(), movie.getYear(), movie.getImage(), movie.getCrew(), movie.getImDbRating());
-    }
-
-    public static MovieFavorites convertMovieDTOToMovieFavorites(MovieDTO movie) {
-        return new MovieFavorites(movie.getId(), movie.getTitle(), movie.getYear(), movie.getImage(), movie.getCrew(), movie.getImDbRating());
     }
 
     public static MovieDB convertMovieDTOToMovieDB(MovieDTO movie) {
