207f30b6c94f11d616911746222c51ff7fd92125
Denis Shesternyak <shesternyakdenis@gmail.com>
2023-06-10 16:20:45 +0300

    Refactoring 'MovieControllerTest'

--- src/test/java/com/shesternyak/myimdbapi/controller/MovieControllerTest.java
+++ src/test/java/com/shesternyak/myimdbapi/controller/MovieControllerTest.java
@@ -1,8 +1,10 @@
 package com.shesternyak.myimdbapi.controller;
 
 import com.shesternyak.myimdbapi.domain.Movie;
+import com.shesternyak.myimdbapi.domain.MovieDB;
 import com.shesternyak.myimdbapi.dto.MovieDTO;
-import com.shesternyak.myimdbapi.service.MoviesService;
+import com.shesternyak.myimdbapi.service.MovieService;
+import com.shesternyak.myimdbapi.service.MoviesFromIMDdService;
 import com.shesternyak.myimdbapi.system.Convertor;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -18,90 +20,91 @@ import static org.mockito.Mockito.when;
 class MovieControllerTest {
 
     @Test
-    void getTestMoviesTest() throws Exception {
-        MoviesService mockMoviesService = mock(MoviesService.class);
-        MovieController controller = new MovieController(mockMoviesService);
-
-        Model mockModel = mock(Model.class);
-        List<Movie> fakeMovies = createFakeMovies();
-        List<MovieDTO> movieDTO = Convertor.convertMovieToMovieDTO(fakeMovies);
-        String viewName = controller.testView(mockModel);
-
-        assertEquals("list-movies", viewName);
-        Mockito.verify(mockModel).addAttribute("movies", movieDTO);
-        Mockito.verify(mockModel).addAttribute("title", "Test movies");
-    }
-
-    @Test
     void getMostPopularMoviesTest() throws Exception {
-        MoviesService mockMoviesService = mock(MoviesService.class);
-        MovieController controller = new MovieController(mockMoviesService);
-
         Model mockModel = mock(Model.class);
-        List<Movie> fakeMovies = createFakeMovies();
-        List<MovieDTO> movieDTO = Convertor.convertMovieToMovieDTO(fakeMovies);
+        MoviesFromIMDdService imDdService = mock(MoviesFromIMDdService.class);
+        MovieService mockMoviesService = mock(MovieService.class);
 
-        when(mockMoviesService.getMostPopularMovies()).thenReturn(fakeMovies);
+        MovieController controller = new MovieController(imDdService, mockMoviesService);
+
+        List<Movie> fakeMovies = createFakeMovies();
+        List<MovieDB> fakeMoviesDB = createFakeMoviesDB();
+
+        List<MovieDTO> tempList = Convertor.convertMovieToMovieDTO(fakeMovies);
+
+        when(imDdService.getMostPopularMovies()).thenReturn(fakeMovies);
+        when(mockMoviesService.getAllMovies()).thenReturn(fakeMoviesDB);
 
         String viewName = controller.getMostPopularMovies(mockModel);
 
-        assertEquals("list-movies", viewName);
-        Mockito.verify(mockModel).addAttribute("movies", movieDTO);
-        Mockito.verify(mockModel).addAttribute("title", "Most popular movies");
+        assertEquals("most-popular-movies", viewName);
+        Mockito.verify(mockModel).addAttribute("movies", tempList);
     }
 
     @Test
     void getMostPopularSeries() throws Exception {
-        MoviesService mockMoviesService = mock(MoviesService.class);
-        MovieController controller = new MovieController(mockMoviesService);
-
         Model mockModel = mock(Model.class);
-        List<Movie> fakeMovies = createFakeMovies();
-        List<MovieDTO> movieDTO = Convertor.convertMovieToMovieDTO(fakeMovies);
+        MoviesFromIMDdService imDdService = mock(MoviesFromIMDdService.class);
+        MovieService mockMoviesService = mock(MovieService.class);
 
-        when(mockMoviesService.getMostPopularSeries()).thenReturn(fakeMovies);
+        MovieController controller = new MovieController(imDdService, mockMoviesService);
+
+        List<Movie> fakeMovies = createFakeMovies();
+        List<MovieDB> fakeMoviesDB = createFakeMoviesDB();
+
+        List<MovieDTO> tempList = Convertor.convertMovieToMovieDTO(fakeMovies);
+
+        when(imDdService.getMostPopularSeries()).thenReturn(fakeMovies);
+        when(mockMoviesService.getAllMovies()).thenReturn(fakeMoviesDB);
 
         String viewName = controller.getMostPopularSeries(mockModel);
 
-        assertEquals("list-movies", viewName);
-        Mockito.verify(mockModel).addAttribute("movies", movieDTO);
-        Mockito.verify(mockModel).addAttribute("title", "Most popular series");
+        assertEquals("most-popular-series", viewName);
+        Mockito.verify(mockModel).addAttribute("movies", tempList);
     }
 
     @Test
     void getTop250Movies() throws Exception {
-        MoviesService mockMoviesService = mock(MoviesService.class);
-        MovieController controller = new MovieController(mockMoviesService);
-
         Model mockModel = mock(Model.class);
-        List<Movie> fakeMovies = createFakeMovies();
-        List<MovieDTO> top250DTO = Convertor.convertMovieToMovieDTO(fakeMovies);
+        MoviesFromIMDdService imDdService = mock(MoviesFromIMDdService.class);
+        MovieService mockMoviesService = mock(MovieService.class);
 
-        when(mockMoviesService.getTop250Movies()).thenReturn(fakeMovies);
+        MovieController controller = new MovieController(imDdService, mockMoviesService);
+
+        List<Movie> fakeMovies = createFakeMovies();
+        List<MovieDB> fakeMoviesDB = createFakeMoviesDB();
+
+        List<MovieDTO> tempList = Convertor.convertMovieToMovieDTO(fakeMovies);
+
+        when(imDdService.getTop250Movies()).thenReturn(fakeMovies);
+        when(mockMoviesService.getAllMovies()).thenReturn(fakeMoviesDB);
 
         String viewName = controller.getTop250Movies(mockModel);
 
-        assertEquals("list-movies", viewName);
-        Mockito.verify(mockModel).addAttribute("movies", top250DTO);
-        Mockito.verify(mockModel).addAttribute("title", "Top 250 movies");
+        assertEquals("top-250-movies", viewName);
+        Mockito.verify(mockModel).addAttribute("movies", tempList);
     }
 
     @Test
     void getTop250Series() throws Exception {
-        MoviesService mockMoviesService = mock(MoviesService.class);
-        MovieController controller = new MovieController(mockMoviesService);
-
         Model mockModel = mock(Model.class);
-        List<Movie> fakeMovies = createFakeMovies();
-        List<MovieDTO> top250DTO = Convertor.convertMovieToMovieDTO(fakeMovies);
+        MoviesFromIMDdService imDdService = mock(MoviesFromIMDdService.class);
+        MovieService mockMoviesService = mock(MovieService.class);
 
-        when(mockMoviesService.getTop250Series()).thenReturn(fakeMovies);
+        MovieController controller = new MovieController(imDdService, mockMoviesService);
+
+        List<Movie> fakeMovies = createFakeMovies();
+        List<MovieDB> fakeMoviesDB = createFakeMoviesDB();
+
+        List<MovieDTO> tempList = Convertor.convertMovieToMovieDTO(fakeMovies);
+
+        when(imDdService.getTop250Series()).thenReturn(fakeMovies);
+        when(mockMoviesService.getAllMovies()).thenReturn(fakeMoviesDB);
 
         String viewName = controller.getTop250Series(mockModel);
 
-        assertEquals("list-movies", viewName);
-        Mockito.verify(mockModel).addAttribute("movies", top250DTO);
-        Mockito.verify(mockModel).addAttribute("title", "Top 250 series");
+        assertEquals("top-250-series", viewName);
+        Mockito.verify(mockModel).addAttribute("movies", tempList);
     }
 
     private List<Movie> createFakeMovies() {
@@ -111,6 +114,19 @@ class MovieControllerTest {
         Movie movie4 = new Movie("tt1517268", "Barbie", 2023, "https://m.media-amazon.com/images/M/MV5BOWIwZGY0OTYtZjUzYy00NzRmLTg5YzgtYWMzNWQ0MmZiY2MwXkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_UX300_CR0,11,300,400_AL_.jpg", "Greta Gerwig (dir.), Margot Robbie, Ryan Gosling", 0.0f);
 
         List<Movie> movies = new ArrayList<>();
+        movies.add(movie1);
+        movies.add(movie2);
+        movies.add(movie3);
+        movies.add(movie4);
+        return movies;
+    }
+    private List<MovieDB> createFakeMoviesDB() {
+        MovieDB movie1 = new MovieDB("tt5971474", "The Little Mermaid", 2023, "https://m.media-amazon.com/images/M/MV5BYTUxYjczMWUtYzlkZC00NTcwLWE3ODQtN2I2YTIxOTU0ZTljXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_UX300_CR0,11,300,400_AL_.jpg", "Rob Marshall (dir.), Halle Bailey, Jonah Hauer-King", 7.2f);
+        MovieDB movie2 = new MovieDB("tt9362722", "Spider-Man: Across the Spider-Verse", 2023, "https://m.media-amazon.com/images/M/MV5BNzQ1ODUzYjktMzRiMS00ODNiLWI4NzQtOTRiN2VlNTNmODFjXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_UX300_CR0,11,300,400_AL_.jpg", "Joaquim Dos Santos (dir.), Shameik Moore, Hailee Steinfeld", 9.1f);
+        MovieDB movie3 = new MovieDB("tt10366206", "John Wick: Chapter 4", 2023, "https://m.media-amazon.com/images/M/MV5BMDExZGMyOTMtMDgyYi00NGIwLWJhMTEtOTdkZGFjNmZiMTEwXkEyXkFqcGdeQXVyMjM4NTM5NDY@._V1_UX300_CR0,11,300,400_AL_.jpg", "Chad Stahelski (dir.), Keanu Reeves, Laurence Fishburne", 8.0f);
+        MovieDB movie4 = new MovieDB("tt1517268", "Barbie", 2023, "https://m.media-amazon.com/images/M/MV5BOWIwZGY0OTYtZjUzYy00NzRmLTg5YzgtYWMzNWQ0MmZiY2MwXkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_UX300_CR0,11,300,400_AL_.jpg", "Greta Gerwig (dir.), Margot Robbie, Ryan Gosling", 0.0f);
+
+        List<MovieDB> movies = new ArrayList<>();
         movies.add(movie1);
         movies.add(movie2);
         movies.add(movie3);
