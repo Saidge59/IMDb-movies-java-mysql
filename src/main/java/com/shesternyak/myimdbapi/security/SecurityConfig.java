4e8ceb5651109108d4e9047453d76172e6a4ccaa
Denis Shesternyak <shesternyakdenis@gmail.com>
2023-07-10 12:30:47 +0300

    Changing 'SecurityConfig.java'

--- src/main/java/com/shesternyak/myimdbapi/security/SecurityConfig.java
+++ src/main/java/com/shesternyak/myimdbapi/security/SecurityConfig.java
@@ -26,6 +26,7 @@ public class SecurityConfig {
                         (requests) -> requests
                                 .requestMatchers(
                                         "/",
+                                        "/css/**",
                                         "/test-movie",
                                         "/registration",
                                         "/most-popular-movies",
